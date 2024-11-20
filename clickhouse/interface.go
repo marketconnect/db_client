da1edce6cfdbbfc5a377eeaeccb8d11e576b2075
marketconnect <ipbogachenko@yandex.ru>
2024-11-20 13:57:09 +0300

    a

--- clickhouse/interface.go
+++ clickhouse/interface.go
@@ -9,4 +9,5 @@ type ClickHouseClient interface {
 	Query(ctx context.Context, sql string, args ...interface{}) (*sql.Rows, error)
 	QueryRow(ctx context.Context, sql string, args ...interface{}) *sql.Row
 	Exec(ctx context.Context, sql string, args ...interface{}) (sql.Result, error)
+	Close() error
 }
